0f0bc12105a455f06f11735b02fb42d132d03966
Coagudream <3025898456@qq.com>
2025-03-26 13:30:00 +0800

    修复商店BUG

--- 商店/store.gd
+++ 商店/store.gd
@@ -32,26 +32,13 @@ func _ready() -> void:
 	refresh_buuton.text = "刷新(%s)" %refresh_gem_amount
 	gam_stone_updata_amount()
 
+
 ##展示商店
 func show_store() -> void:
 	v_2.player = player
 	refresh_items()
 	show()
 
-##判断能不能买
-func can_or_notcan_buy() -> void:
-	if not run_state:
-		return
-	for item in items_container.get_children():
-		if item.items.gam_stone_spend > run_state.run_state_.gem_amount:
-			item.buy_botton.disabled = true
-
-
-# BUG 刷新无法及时更新（恢复）按下状态
-##能不能按下刷新按钮
-func refresh_button_diab() -> void:
-	if refresh_gem_amount > run_state.run_state_.gem_amount:
-		refresh_buuton.disabled = true
 
 ##刷新逻辑更新
 func refresh_updata() -> void:
@@ -59,7 +46,7 @@ func refresh_updata() -> void:
 	refresh_gem_amount += int(25*(log(refresh_times+3)/log(10)))
 	refresh_times += 1
 	run_state.run_state_.gem_amount -= refresh_gem_amount
-	refresh_button_diab()
+	updata_button_atate()
 
 ##刷新道具卡牌
 func refresh_items() -> void:
@@ -68,33 +55,54 @@ func refresh_items() -> void:
 			item_lock += 1
 			continue
 		child.queue_free()
-	
 	for item in range(ITEMS_AMOUNT - item_lock):
 		var new_item := ITEMS.instantiate()
 		new_item.items = text_effect()
 		new_item.player = player
 		new_item.run_state = run_state
-		new_item.already_is_buy.connect(can_or_notcan_buy)
+		new_item.already_is_buy.connect(updata_button_atate)
 		items_container.add_child(new_item)
-	can_or_notcan_buy()
+	updata_button_atate()
 	item_lock = 0
 
+##更新按钮状态
+func updata_button_atate() -> void:
+	can_or_not_can_refresh()
+	can_or_not_can_buy()
 
+##能不能按下刷新按钮
+func can_or_not_can_refresh() -> void:
+	if refresh_gem_amount > run_state.run_state_.gem_amount:
+		refresh_buuton.disabled = true
+	else :
+		refresh_buuton.disabled = false
+
+##判断能不能按下购买按钮
+func can_or_not_can_buy() -> void:
+	if not run_state:
+		return
+	for item in items_container.get_children():
+		if item.items.gam_stone_spend > run_state.run_state_.gem_amount:
+			item.buy_botton.disabled = true
+		else:
+			item.buy_botton.disabled = false
+	can_or_not_can_refresh()
+
+
+#测试用
 func text_effect() -> Resource:
 	var amount : int = randi_range(0,1)
 	if  amount == 1:
 		return FIRE_TIME_DOWN
 	else:
 		return COLL_DAMAGE_UP
-	
 
-
+##更新ui
+func gam_stone_updata_amount() -> void:
+	gem_amount.text = str(run_state.run_state_.gem_amount)
+	updata_button_atate()
 
 ##请求下一个回合
 func _on_next_round_pressed() -> void:
 	Events.request_next_round_start.emit()
 	hide()
-
-##更新ui
-func gam_stone_updata_amount() -> void:
-	gem_amount.text = str(run_state.run_state_.gem_amount)
